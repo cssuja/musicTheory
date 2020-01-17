9d710785a45d2c53aba1e1fe5864f9f0a09226f8
Ashford M.R <m.r.ashford@swansea.ac.uk>
2020-01-17 10:36:44 +0000

    Real data!

--- MusicTheory/Features/Question/LessonRepository.cs
+++ MusicTheory/Features/Question/LessonRepository.cs
@@ -25,84 +25,108 @@ namespace MusicTheory.Features.Question
 
         public Lesson GetLesson(int lessonId)
         {
+            Lesson lesson;
+            IList<QuestionModel> questions;
             using (var cnn = new SqlConnection(_config.ConnectionStrings.MusicTheoryConnectionString))
             {
                 cnn.Open();
+
                 using(var t = cnn.BeginTransaction())
                 {
-                    var sql = @"
+                    var lessonSql = @"
 select * from Lessons
+where Id = @lessonId
 ";
 
-                    var x = cnn.Query<Lesson>(sql, transaction: t).FirstOrDefault();
+                    lesson = cnn.Query<Lesson>(lessonSql, new { lessonId }, transaction: t).FirstOrDefault();
+
+                    var questionSql = @"
+select Id, Text as QuestionText, AnswerOptionId as AnswerId from Questions
+where Id in (select QuestionId from LessonQuestions where LessonId = @lessonId)
+";
+                    questions = cnn.Query<QuestionModel>(questionSql, new { lessonId }, transaction: t).ToList();
+
+                    var textOptionsSql = @"
+select TextOptions.Id, TextOptions.Text from TextOptions 
+inner join QuestionOptions on TextOptions.Id = QuestionOptions.OptionId
+where QuestionOptions.QuestionId = @questionId
+";
+
+                    foreach(var question in questions)
+                    {
+                        var options = cnn.Query<TextQuestionOption>(textOptionsSql, new { questionId = question.Id }, transaction: t).ToList();
+                        question.TextOptions = options;
+                    }
+
+                    lesson.Questions = questions;
                 }
             }
 
-            var options = new List<TextQuestionOption> {
-                new TextQuestionOption {
-                    Id = 1,
-                    QuestionId = lessonId,
-                    Text="Option1"
-                },
-                new TextQuestionOption {
-                    Id = 2,
-                    QuestionId = lessonId,
-                    Text="Option2"
-                },
-                new TextQuestionOption {
-                    Id = 3,
-                    QuestionId = lessonId,
-                    Text="Option3"
-                },
-                new TextQuestionOption {
-                    Id = 4,
-                    QuestionId = lessonId,
-                    Text="Option4"
-                },
-            };
+            //var options = new List<TextQuestionOption> {
+            //    new TextQuestionOption {
+            //        Id = 1,
+            //        QuestionId = lessonId,
+            //        Text="Option1"
+            //    },
+            //    new TextQuestionOption {
+            //        Id = 2,
+            //        QuestionId = lessonId,
+            //        Text="Option2"
+            //    },
+            //    new TextQuestionOption {
+            //        Id = 3,
+            //        QuestionId = lessonId,
+            //        Text="Option3"
+            //    },
+            //    new TextQuestionOption {
+            //        Id = 4,
+            //        QuestionId = lessonId,
+            //        Text="Option4"
+            //    },
+            //};
 
-            var lesson = new Lesson
-            {
-                Id = lessonId,
-                Name = "Major Scale",
-                Questions = new List<QuestionModel> {
-                    new QuestionModel
-                    {
-                        Id = lessonId,
-                        QuestionText = "This is the first question",
-                        TextOptions = options,
-                        AnswerId = 2
-                    },
-                    new QuestionModel
-                    {
-                        Id = lessonId,
-                        QuestionText = "This is the second question",
-                        TextOptions = options,
-                        AnswerId = 3
-                    },
-                    new QuestionModel
-                    {
-                        Id = lessonId,
-                        QuestionText = "This is the third question",
-                        TextOptions = options,
-                        AnswerId = 1
-                    },
-                    new QuestionModel
-                    {
-                        Id = lessonId,
-                        QuestionText = "This is the fourth question",
-                        TextOptions = options,
-                        AnswerId = 3
-                    },
-                    new QuestionModel
-                    {
-                        Id = lessonId,
-                        QuestionText = "This is the fifth question",
-                        TextOptions = options,
-                        AnswerId = 1
-                    }
-                }
-            };
+            //var lesson = new Lesson
+            //{
+            //    Id = lessonId,
+            //    Name = "Major Scale",
+            //    Questions = new List<QuestionModel> {
+            //        new QuestionModel
+            //        {
+            //            Id = lessonId,
+            //            QuestionText = "This is the first question",
+            //            TextOptions = options,
+            //            AnswerId = 2
+            //        },
+            //        new QuestionModel
+            //        {
+            //            Id = lessonId,
+            //            QuestionText = "This is the second question",
+            //            TextOptions = options,
+            //            AnswerId = 3
+            //        },
+            //        new QuestionModel
+            //        {
+            //            Id = lessonId,
+            //            QuestionText = "This is the third question",
+            //            TextOptions = options,
+            //            AnswerId = 1
+            //        },
+            //        new QuestionModel
+            //        {
+            //            Id = lessonId,
+            //            QuestionText = "This is the fourth question",
+            //            TextOptions = options,
+            //            AnswerId = 3
+            //        },
+            //        new QuestionModel
+            //        {
+            //            Id = lessonId,
+            //            QuestionText = "This is the fifth question",
+            //            TextOptions = options,
+            //            AnswerId = 1
+            //        }
+            //    }
+            //};
 
             return lesson;
         }
--- MusicTheory/Features/Question/Models/QuestionOption.cs
+++ MusicTheory/Features/Question/Models/QuestionOption.cs
@@ -8,18 +8,15 @@ namespace MusicTheory.Features.Question.Models
     public interface IQuestionOption
     {
         public int Id { get; set; }
-        public int QuestionId { get; set; }
     }
     public class TextQuestionOption :  IQuestionOption
     {
         public int Id { get; set; }
-        public int QuestionId { get; set; }
         public string Text { get; set; }
     }
     public class ImageQuestionOption : IQuestionOption
     {
         public int Id { get; set; }
-        public int QuestionId { get; set; }
         public byte[] Image { get; set; }
     }
 }
