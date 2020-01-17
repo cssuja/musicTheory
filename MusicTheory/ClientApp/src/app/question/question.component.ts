31fb9284b4ad596c014e42de859d34f6c3369e55
Ashford M.R <m.r.ashford@swansea.ac.uk>
2020-01-17 11:17:23 +0000

    grey out check button

--- MusicTheory/ClientApp/src/app/question/question.component.ts
+++ MusicTheory/ClientApp/src/app/question/question.component.ts
@@ -5,7 +5,7 @@ import { QuestionService } from './question.service';
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
   lesson: Lesson;
